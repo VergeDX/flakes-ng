192aa6680a131dc5a999735e3c81342a16aa47c4
Vanilla <osu_Vanilla@126.com>
2022-01-28 18:05:06 +0800

    feat[flake]: Switch to NixOS unstable channel.

--- flake.nix
+++ flake.nix
@@ -1,18 +1,14 @@
 {
   inputs = {
-    nixos-stable.url = "github:nixos/nixpkgs/nixos-21.11";
+    nixos.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-
-    home-manager.url = "github:nix-community/home-manager/release-21.11";
-    nixos-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.inputs.nixpkgs.follows = "nixos-unstable";
-
+    home-manager.url = "github:nix-community/home-manager";
     impermanence.url = "github:nix-community/impermanence";
   };
 
   outputs = { self, ... }@inputs: with inputs;
     flake-utils.lib.eachDefaultSystem (system: {
-      packages.nixosConfigurations."NixOS-RoT" = nixos-stable.lib.nixosSystem rec {
+      packages.nixosConfigurations."NixOS-RoT" = nixos.lib.nixosSystem rec {
         inherit system;
         modules = [ ./configuration.nix ] ++ [ home-manager.nixosModules.home-manager ]
           ++ [{ home-manager.users."vanilla" = import ./home-manager/home.nix; }]
