fe9a2c3f309d79293cde89231778d54b11e2de3e
System administrator <root@NixOS-Laptop.vanilla.local>
2022-01-28 11:12:01 +0800

    feat[hm]: Add NixOS home-manager module for user vanilla.

--- flake.nix
+++ flake.nix
@@ -2,6 +2,10 @@
   inputs = {
     nixos-stable.url = "github:nixos/nixpkgs/nixos-21.11";
     flake-utils.url = "github:numtide/flake-utils";
+
+    home-manager.url = "github:nix-community/home-manager/release-21.11";
+    nixos-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.inputs.nixpkgs.follows = "nixos-unstable";
   };
 
   outputs = { self, ... }@inputs: with inputs;
@@ -9,7 +13,10 @@
       packages = {
         nixosConfigurations."NixOS-RoT" = nixos-stable.lib.nixosSystem rec {
           inherit system;
-          modules = [ ./configuration.nix ];
+          modules = [ ./configuration.nix ] ++ [
+            home-manager.darwinModules.home-manager
+            { home-manager.users."vanilla" = import ./home.nix; }
+          ];
         };
       };
     });
