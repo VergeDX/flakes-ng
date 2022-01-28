f66e9c30b83032519cb8582497a53e4531d4c3cf
Vanilla <osu_Vanilla@126.com>
2022-01-28 04:58:42 +0000

    style[hardware]: Make hardware folder, move to $_.

--- configuration.nix
+++ configuration.nix
@@ -8,8 +8,8 @@
   imports =
     [
       # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ./fileSystems.nix
+      ./hardware/configuration.nix
+      ./hardware/fileSystems.nix
 
       ./users.nix
 
