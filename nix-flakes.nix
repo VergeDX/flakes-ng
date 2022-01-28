0f378e1409766094bbbc5a13d0b72bb7c9f1a4b1
Vanilla <osu_Vanilla@126.com>
2022-01-28 03:46:38 +0000

    fix[nix-flakes]: Add git and config, for nix flake commands.

--- nix-flakes.nix
+++ nix-flakes.nix
@@ -2,4 +2,10 @@
 {
   nix.package = pkgs.nixFlakes;
   nix.extraOptions = "experimental-features = nix-command flakes";
+
+  programs.git.enable = true;
+  programs.git.config = {
+    user.name = "Vanilla";
+    user.email = "osu_Vanilla@126.com";
+  };
 }
