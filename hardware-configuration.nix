445d1f8d69040dcd9cbad07c0e2048eec6f938cf
System administrator <root@NixOS-Laptop.vanilla.local>
2022-01-28 10:12:16 +0800

    style[.]: Do nixpkgs-fmt on ., version 1.2.0.

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -5,7 +5,8 @@
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "rtsx_pci_sdmmc" ];
@@ -14,36 +15,40 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "none";
+    {
+      device = "none";
       fsType = "tmpfs";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/D8F2-F6CB";
+    {
+      device = "/dev/disk/by-uuid/D8F2-F6CB";
       fsType = "vfat";
     };
 
   fileSystems."/nix" =
-    { device = "/dev/disk/by-uuid/e03e0ab5-1871-425c-a9ae-801fa68ef45c";
+    {
+      device = "/dev/disk/by-uuid/e03e0ab5-1871-425c-a9ae-801fa68ef45c";
       fsType = "btrfs";
       options = [ "subvol=nix" ];
     };
 
   fileSystems."/persistent" =
-    { device = "/dev/disk/by-uuid/e03e0ab5-1871-425c-a9ae-801fa68ef45c";
+    {
+      device = "/dev/disk/by-uuid/e03e0ab5-1871-425c-a9ae-801fa68ef45c";
       fsType = "btrfs";
       options = [ "subvol=persistent" ];
     };
 
   fileSystems."/etc/nixos" =
-    { device = "/persistent/persistent/Projects/flakes-ng";
+    {
+      device = "/persistent/persistent/Projects/flakes-ng";
       fsType = "none";
       options = [ "bind" ];
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/1b07ef58-fa1b-4e5e-a394-7a07a9229f07"; }
-    ];
+    [{ device = "/dev/disk/by-uuid/1b07ef58-fa1b-4e5e-a394-7a07a9229f07"; }];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
