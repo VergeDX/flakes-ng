020ea0c5271634e0b9e09c0d87642ecd5afad85a
Vanilla <osu_Vanilla@126.com>
2022-01-28 09:08:21 +0000

    perf[chromium]: Persist config for .config/chromium.

--- home-manager/programs/chromium.nix
+++ home-manager/programs/chromium.nix
@@ -1,8 +1,12 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   programs.chromium.enable = true;
   systemd.user.sessionVariables = {
     GOOGLE_DEFAULT_CLIENT_ID = "77185425430.apps.googleusercontent.com";
     GOOGLE_DEFAULT_CLIENT_SECRET = "OTJgUOQcT7lO7GsGZq2G4IlT";
   };
+
+  home.file.".config/chromium".source =
+    config.lib.file.mkOutOfStoreSymlink
+      "/persistent/dot-config/chromium";
 }
