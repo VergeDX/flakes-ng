4a12f183bba58809e5ef4a5034654ef505a12073
System administrator <root@NixOS-Laptop.vanilla.local>
2022-01-28 10:31:05 +0800

    feat[users]: Enable immutableUsers, add normal user vanilla.

---
+++ users.nix
@@ -0,0 +1,10 @@
+{ pkgs, ... }:
+{
+  users.mutableUsers = false;
+  users.defaultUserShell = pkgs.fish;
+
+  users.users."vanilla" = {
+    isNormalUser = true;
+    hashedPassword = "$6$rootfs/on/tmpfs$ZdJgQnzRONBdzI0TKoRaXVf5jgvWZMfZL4faLLc1ACMtHuTTzEogZhyBLKB6MsWztkSC6FHygH.6O/o37Pd1p0";
+  };
+}
