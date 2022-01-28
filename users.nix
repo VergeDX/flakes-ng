85cd528850700d9b4122e34672211a004c870270
System administrator <root@NixOS-Laptop.vanilla.local>
2022-01-28 10:38:54 +0800

    fix[users]: Add user vanilla to groups wheel.

--- users.nix
+++ users.nix
@@ -6,5 +6,6 @@
   users.users."vanilla" = {
     isNormalUser = true;
     hashedPassword = "$6$rootfs/on/tmpfs$ZdJgQnzRONBdzI0TKoRaXVf5jgvWZMfZL4faLLc1ACMtHuTTzEogZhyBLKB6MsWztkSC6FHygH.6O/o37Pd1p0";
+    extraGroups = [ "wheel" ];
   };
 }
