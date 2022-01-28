d550d530c4af76b5df5e273388f3932bea6aae5c
System administrator <root@NixOS-Laptop.vanilla.local>
2022-01-28 11:18:42 +0800

    perf[impermanence]: mount /persistent/Projects for user vanilla.

--- home.nix
+++ home.nix
@@ -1,5 +1,6 @@
 { inputs, ... }: with inputs;
 {
-  imports = [ impermanence.nixosModules.home-manager.impermanence ];
+  imports = [ impermanence.nixosModules.home-manager.impermanence ]
+    ++ [ ./impermanence.nix ];
   home.stateVersion = "21.11";
 }
