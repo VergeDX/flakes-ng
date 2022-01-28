aaf457f2a5b22142d5cfe7cf7472e6492a38749e
Vanilla <osu_Vanilla@126.com>
2022-01-28 09:18:32 +0000

    feat[file-links]: persist .cache/nix, nix run no more wait.

--- home-manager/file-links.nix
+++ home-manager/file-links.nix
@@ -3,4 +3,8 @@
   home.file.".config/monitors.xml".source =
     config.lib.file.mkOutOfStoreSymlink
       "/persistent/dot-config/monitors.xml";
+
+  home.file.".cache/nix".source =
+    config.lib.file.mkOutOfStoreSymlink
+      "/persistent/dot-cache/nix";
 }
