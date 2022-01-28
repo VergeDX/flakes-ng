c65523f88155aded0ed71cf9aa83293fcaf251f1
System administrator <root@NixOS-Laptop.vanilla.local>
2022-01-28 11:08:15 +0800

    style[flake]: Use outer scope inputs, instead of prefix.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
-  inputs.nixos-stable.url = "github:nixos/nixpkgs/nixos-21.11";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixos-stable.url = "github:nixos/nixpkgs/nixos-21.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
   outputs = { self, ... }@inputs: with inputs;
     flake-utils.lib.eachDefaultSystem (system: {
