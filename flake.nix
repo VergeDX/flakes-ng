381bebe62ba27261b265fe71fc1963acd927ffe8
System administrator <root@NixOS-Laptop.vanilla.local>
2022-01-28 10:56:10 +0800

    feat[flake]: Add flake.nix, use NixOS Flakes build system.

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  inputs.nixos-stable.url = "github:nixos/nixpkgs/nixos-21.11";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, ... }@inputs: with inputs;
+    flake-utils.lib.eachDefaultSystem (system: {
+      packages = {
+        nixosConfigurations."NixOS-RoT" = nixos-stable.lib.nixosSystem rec {
+          inherit system;
+          modules = [ ./configuration.nix ];
+        };
+      };
+    });
+}
