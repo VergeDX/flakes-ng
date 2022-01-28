f40b3fde031041c2e0b91dfa617fc229dd7cde3f
Vanilla <osu_Vanilla@126.com>
2022-01-28 06:28:12 +0000

    perf[hm]: link .config/nixpkgs to Projects/flakes-ng/home-manager.

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,6 +1,11 @@
-{ inputs, ... }: with inputs;
+{ inputs, config, ... }: with inputs;
 {
   imports = [ impermanence.nixosModules.home-manager.impermanence ]
     ++ [ ./impermanence.nix ];
+
+  home.file.".config/nixpkgs".source =
+    config.lib.file.mkOutOfStoreSymlink
+      "/home/vanilla/Projects/flakes-ng/home-manager";
+
   home.stateVersion = "21.11";
 }
