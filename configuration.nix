328a6a34069ff1b7c8f730e18d37888ce92656f7
Vanilla <osu_Vanilla@126.com>
2022-01-28 05:25:04 +0000

    style[global]: Move xserver config -> $_.nix.

--- configuration.nix
+++ configuration.nix
@@ -12,6 +12,7 @@
       ./hardware/fileSystems.nix
 
       ./users.nix
+      ./xserver.nix
 
       ./nix/flakes.nix
     ];
@@ -44,15 +45,6 @@
   #   font = "Lat2-Terminus16";
   #   keyMap = "us";
   # };
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
 
   # Configure keymap in X11
   # services.xserver.layout = "us";
