271488e02b8da104ce1d203928a3c85ab6e62ec4
Vanilla <osu_Vanilla@126.com>
2022-01-28 05:32:12 +0000

    perf[gnome]: Disable core-utilities and developer-tools.

--- xserver.nix
+++ xserver.nix
@@ -6,4 +6,8 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+
+  # NixOS/nixpkgs - nixos/modules/services/x11/desktop-managers/gnome.nix
+  services.gnome.core-utilities.enable = false;
+  services.gnome.core-developer-tools.enable = false;
 }
