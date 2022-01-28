5b045cd2e51c279693aa17fe84d72a46c24a1548
Vanilla <osu_Vanilla@126.com>
2022-01-28 09:19:46 +0000

    feat[boot]: Switch to XanMod kernel, enable sysrq.

--- boot.nix
+++ boot.nix
@@ -1,6 +1,9 @@
-{ ... }:
+{ pkgs, ... }:
 {
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.kernelPackages = pkgs.linuxPackages_xanmod;
+  boot.kernel.sysctl = { "kernel.sysrq" = 1; };
 }
